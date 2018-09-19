7d7d5bca9e4dd4050489319492776a96956e85eb
Al <xochipillitx@gmail.com>
2018-09-19 02:20:42 -0500

    Added yelp endpoints, requests lib, default values, req function

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, current_app
 from flask import request, jsonify
 from app.main import bp
-from app.main.config import API_KEY_YELP
+from app.main.config import headers
+import requests
 
 '''
 TO DO:
@@ -26,9 +27,30 @@ Gets business details from Yelp using the provided ID. Return the full response 
 
 '''
 
+# Defaults
+LONG = "30.267153"
+LAT = "-97.743057"
+RADIUS = "1609" # Meters
+FOOD_CAT = "&categories="
+FOOD_CAT_OP = ""
+BUSNS_ID = "LFVtF9FeWZ3mavVPk0j1Gw"
+
+# Yelp Endpoints
+ALL_US_CATEGORIES_URL = "https://api.yelp.com/v3/categories?locale=en_US"
+SEARCH_URL = "https://api.yelp.com/v3/businesses/search?latitude=" + LONG + "&longitude=" + LAT + "&limit=10&radius=" + RADIUS
+BUSNS_DET_URL = "https://api.yelp.com/v3/businesses/" + BUSNS_ID
+
+# Function for Requests
+def getJSON(url, auth):
+    r = requests.get(url, headers=auth)
+    data = r.json()
+    return data
+
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    data = getJSON(ALL_US_CATEGORIES_URL, headers)
+    resp = jsonify(data)
+    return resp
 
 @bp.route('/yelp/categories')
 def yelpCat():
