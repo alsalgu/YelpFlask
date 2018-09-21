08a1103e2b7f5bf2d0aa73ebd6e0f40a21268510
Al <xochipillitx@gmail.com>
2018-09-21 05:58:25 -0500

    Requirements added, testing for form post fail/success added

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
 from flask import url_for
+from app.main.forms import businessIDForm
+from app.main.forms import searchYelpForm
 
 def test_routes(client):
     routes = {"GET":['main.index',
@@ -7,9 +9,11 @@ def test_routes(client):
                      'main.yelpSingBus',
                      'main.businessDetails'], 
               "POST":['main.yelpBus',
-                      'main.businessDetails']}
+                      'main.businessDetails'],
+              "404":['/fake-path',
+                     '/yelp/businesses/details/fake-id']}
     valid_ID = "LFVtF9FeWZ3mavVPk0j1Gw"
-    
+
     for route in routes["GET"]:
         if route == 'main.yelpSingBus':
             assert client.get(url_for(route, id=valid_ID)).status_code == 200
@@ -17,3 +21,39 @@ def test_routes(client):
             assert client.get(url_for(route)).status_code == 200
     for route in routes["POST"]:
         assert client.post(url_for(route)).status_code == 200
+    for route in routes["404"]:
+        assert client.get(route).status_code == 302
+
+def test_ID_post(client):
+    form = businessIDForm(business_id="LFVtF9FeWZ3mavVPk0j1Gw",
+                          viewBusiness=True)
+    resp = client.post(url_for('main.businessDetails'), follow_redirects=True, data=form.data)
+    assert "data" in resp.json
+
+def test_search_post(client):
+    form = searchYelpForm(latitude='30.2672',
+                          longitude='-97.7431',
+                          radius='1609',
+                          category='bbq',
+                          search=True)
+    resp = client.post(url_for('main.yelpBus'), follow_redirects=True, data=form.data)
+    assert "data" in resp.json
+
+def test_ID_post_fail(client):
+    form = businessIDForm(business_id="fake-id",
+                          viewBusiness=True)
+    resp = client.post(url_for('main.businessDetails'), follow_redirects=True, data=form.data)
+    assert b'Sorry there was an error' in resp.data
+
+def test_search_post_fail(client):
+    form = searchYelpForm(latitude='300.2672',
+                          longitude='-999.7431',
+                          radius='1609',
+                          category='bbq',
+                          search=True)
+    resp = client.post(url_for('main.yelpBus'), follow_redirects=True, data=form.data)
+    assert b'Sorry there was an error' in resp.data
+
+def test_category_JSON(client):
+    resp = client.get(url_for('main.yelpCat'))
+    assert "data" in resp.json
